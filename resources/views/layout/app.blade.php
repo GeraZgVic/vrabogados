029c3ce4ca7bba71eea816555e934f02c7d6a5da
Victor Zúñiga <gerazgvic@gmail.com>
2024-05-08 18:15:41 -0600

    Seguimiento de Vite para Flowbite

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -42,7 +42,7 @@
             font-style: normal;
         }
     </style>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resource/js/flowbite.js'])
 </head>
 
 <body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,3 @@
 @section('contenido')
    <p class="work-sans-normal text-xl">pasfashjfashfkjahsjfe?</p>
 @endsection
-
-@push('script')
-    <script src="{{asset('build/assets/flowbite.min.js')}}"></script>
-@endpush
