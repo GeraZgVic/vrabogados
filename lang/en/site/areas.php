cc21ec8acce2bc21371ca62899e1bc33beae2abf
Carlo Reyes <carlosrey1910@gmail.com>
2024-05-28 10:39:22 -0600

    Se agrego areas en

--- lang/en/site/areas.php
+++ lang/en/site/areas.php
@@ -11,4 +11,7 @@ return [
 
     'areas-servicio-descripcion' => 'We have a deep understanding of our clients needs thanks to a solid experience, continuous training, and personalized treatment, all for the benefit of our clients peace of mind and security.',
     'areas-practicas' => 'AREAS OF PRACTICE',
+
+    // list of areas
+
 ];
