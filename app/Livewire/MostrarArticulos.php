0c443ef93708b4a16a7f3b3698584c4f405b7458
Victor Zúñiga <gerazgvic@gmail.com>
2024-06-18 16:05:10 -0600

    Componente Livewire para Mostrar articulos en el blog, usando livewire, con buscador

--- app/Livewire/MostrarArticulos.php
+++ app/Livewire/MostrarArticulos.php
@@ -2,14 +2,30 @@
 
 namespace App\Livewire;
 
+use App\Models\Article;
 use Livewire\Component;
 
 class MostrarArticulos extends Component
 {
+
+    // #[Url] //Si queremos que muestre la URL
     public $search = '';
 
     public function render()
     {
-        return view('livewire.mostrar-articulos');
+        $query = Article::orderBy('created_at', 'desc');
+
+        // Filtrar los inventarios que coinciden con el término de búsqueda (title)
+        if ($this->search) {
+            $query->where(function ($innerQuery) {
+                $innerQuery->where('title', 'LIKE', "%" . $this->search . "%");
+            });
+        }
+
+        $articles = $query->get();
+
+        return view('livewire.mostrar-articulos', [
+            'articles' => $articles
+        ]);
     }
 }
