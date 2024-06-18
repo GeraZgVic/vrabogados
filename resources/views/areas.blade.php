9a1d990935c00087ce24431ae92ad5f2b0cf9a1a
Carlo Reyes <carlosrey1910@gmail.com>
2024-06-18 15:22:42 -0600

    Se modifico areas de practica || aun no se termina la seccion de informate

--- resources/views/areas.blade.php
+++ resources/views/areas.blade.php
@@ -4,82 +4,194 @@ Areas de práctica
 @endsection
 
 @section('contenido')
-<div class="container m-auto px-8 mt-4 flex flex-col justify-center text-center">
-    <div class="my-4">
-        <h2
-            class="text-3xl font-bold bg-gradient-to-r  from-blue-950 via-blue-800 to-blue-950 inline-block text-transparent bg-clip-text ">
-            AREAS DE EJERCICIO</h2>
-        <div class="mt-4 space-y-6">
-            <p class="text-2xl text-gray-500"">{{__('site/areas.areas-servicio-descripcion')}}</p>
+<div class="flex h-screen items-center justify-center">
+    <div class="font-inter antialiased relative h-full w-full bg-cover bg-center bg-fixed"
+        style="background-image: url('https://media.istockphoto.com/id/1590137991/es/foto/asesor%C3%ADa-legal-presenta-al-cliente-un-contrato-firmado-con-martillo-y-derecho-legal-concepto.jpg?s=1024x1024&w=is&k=20&c=h8AUZUejDCgJjjniV4y2ejWgK2gVuERe6BB0KG9LheM=');">
+        <div class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
+            <div class="p-6 text-white">
+                <h1 class="pb-4 text-3xl md:text-4xl lg:text-6xl mb-2 md:m-0 text-white text-center animate-fade-up">
+                    Servicios Legales</h1>
+                <p class="mb-8 text-lg italic font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-32 animate-bottom">
+                    ¿Necesitas resolver un problema legal? En Vivero Romero, ofrecemos soluciones expertas
+                    en todas las áreas del derecho. Nuestro equipo de abogados especializados está listo para ayudarte.
+                </p>
+            </div>
         </div>
-        <div>
-            <div class=" grid grid-cols-1 md:grid-cols-2 gap-10 my-8">
+    </div>
+</div>
+
+<div class="flex min-h-screen items-center justify-center bg-neutral-800">
+    <div class="mt-5 mb-8 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+        <div
+            class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div class="h-96 w-72">
+                <img class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                    src="https://img.freepik.com/foto-gratis/vista-superior-elementos-orientacion-profesional-jueces_23-2149443471.jpg?t=st=1718736535~exp=1718740135~hmac=831e13b79f44b14ecf5695531ba24b34330f34de79dba9e6b57a63f6d459f206&w=360"
+                    alt="" />
+            </div>
+            <div
+                class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70">
+            </div>
+            <div
+                class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">DERECHO CIVIL</h1>
+                <p
+                    class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.</p>
+                <button
+                    class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">See
+                    More</button>
+            </div>
+        </div>
+        <div
+            class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div class="h-96 w-72">
+                <img class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                    src="https://img.freepik.com/foto-gratis/corte-martillo-libros-juicio-concepto-ley_144627-30453.jpg?t=st=1718736475~exp=1718740075~hmac=83fc57a1463d3eccbea37d72769cf20ec580e6313dd9c6aaec5293650587ef01&w=360"
+                    alt="" />
+            </div>
+            <div
+                class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70">
+            </div>
+            <div
+                class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">DERECHO PENAL</h1>
+                <p
+                    class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.</p>
+                <button
+                    class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">See
+                    More</button>
+            </div>
+        </div>
+        <div
+            class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div class="h-96 w-72">
+                <img class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                    src="https://img.freepik.com/foto-gratis/concepto-collage-opresion-dictadura_23-2149711439.jpg?w=360&t=st=1718736424~exp=1718737024~hmac=de7d44fea01cdd0859b7db12dbf60a70d63993f4c5745d45a9bc4feb203b8e7d"
+                    alt="" />
+            </div>
+            <div
+                class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70">
+            </div>
+            <div
+                class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">DERECHO LABORAL</h1>
+                <p
+                    class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.</p>
+                <button
+                    class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">See
+                    More</button>
+            </div>
+        </div>
+        <div
+            class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div class="h-96 w-72">
+                <img class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                    src="https://img.freepik.com/foto-gratis/abogado-maduro-serio-que-lee-libro-ley-tribunal_23-2147898517.jpg?t=st=1718738180~exp=1718741780~hmac=475595a60a34fecca1c0243be4147213a5cf02d0323dbb674917e9173d252f80&w=360"
+                    alt="" />
+            </div>
+            <div
+                class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70">
+            </div>
+            <div
+                class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">DERECHO ADMINISTRATIVO</h1>
+                <p
+                    class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.</p>
+                <button
+                    class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">See
+                    More</button>
+            </div>
+        </div>
+        <div
+            class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div class="h-96 w-72">
+                <img class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                    src="https://img.freepik.com/foto-gratis/detalles-columna-marmol-gris-edificio_1359-886.jpg?t=st=1718738255~exp=1718741855~hmac=c4d5abb36e226f87e5768f173a6a0ab0d0c1c338f23c20aad562498332c15821&w=740"
+                    alt="" />
+            </div>
+            <div
+                class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70">
+            </div>
+            <div
+                class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">DERECHO MERCANTIL</h1>
+                <p
+                    class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.</p>
+                <button
+                    class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">See
+                    More</button>
+            </div>
+        </div>
+        <div
+            class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+            <div class="h-96 w-72">
+                <img class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                    src="https://img.freepik.com/foto-gratis/bodegon-monedas-dolar-escala_23-2150794349.jpg?t=st=1718738335~exp=1718741935~hmac=602f3bca70c7387f9029f7fe42396830cd49144b0312871b32faf1f54ae1540e&w=360"
+                    alt="" />
+            </div>
+            <div
+                class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70">
+            </div>
+            <div
+                class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                <h1 class="font-dmserif text-3xl font-bold text-white">DERECHO TRIBUTARIO</h1>
+                <p
+                    class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.</p>
+                <button
+                    class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">See
+                    More</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="relative flex flex-col justify-center overflow-hidden bg-red-950 py-6 sm:py-12">
+    <div class="w-full ">
+        <div class="group grid w-full grid-cols-2">
+            <div class="pl-16 relative flex items-end flex-col ">
+                <p class="font-bold text-sm flex mt-2 mb-8 items-center gap-2" href="">
+                    <span class="text-white">Informate</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="#fff" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                    </svg>
+                </p>
+                <div class="rounded-xl overflow-hidden">
+                    <img src="https://img.freepik.com/fotos-premium/grupo-punos-mapa-mundo-detras-union-contra-cancer_1136993-81.jpg?w=740"
+                        alt="">
+                </div>
+            </div>
             <div>
-                <img class="rounded-md"
-                    src="https://media.istockphoto.com/id/956243400/es/foto/cierre-empresario-abogado-trabajando-o-leyendo-el-libro-de-derecho-en-trabajo-de-oficina-para.jpg?s=1024x1024&w=is&k=20&c=0qwlfbmo-KSMAm9agaZwRTab2NDfBRHqk_7UJVi60oU="
-                    alt="Vrabogados">
-            </div>
-            <div class="container mx-auto">
-                <ul class="list-disc text-blue-900">
-                    <li>Litigio en materia administrativa.</li>
-                    <li>Litigio en materia de amparo.</li>
-                    <li>Litigio en materia civil del fuero común.</li>
-                    <li>Litigio en materia civil federal.</li>
-                    <li>Litigio en materia familiar y arrendamiento inmobiliario.</li>
-                    <li>Litigio en materia fiscal.</li>
-                    <li>Litigio en materia mercantil.</li>
-                    <li>Asesoramiento en cada una de las materias antes mencionadas.</li>
-                    <li>Litigio en materia laboral a través de nuestras firmas asociadas.</li>
-                    <li>Litigio en materia penal a través de nuestras firmas asociadas.</li>
-                    <li>Litigio en materia agraria a través de nuestras firmas asociadas.</li>
-                    <li>Litigio en materia corporativa a través de nuestras firmas asociadas.</li>
-                </ul>
-            </div>
-        </div>
-        <div class="grid md:grid-cols-3 gap-10 my-16">
-            <div class="block max-w-sm rounded-lg bg-white bg-cover p-6 shadow-lg dark:bg-neutral-700" style="background-image: url('img/img-3.webp');">
-                <h5 class="mb-2 text-xl font-medium leading-tight text-white ">
-                    Card title
-                </h5>
-                <p class="mb-4 text-base text-white">
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </p>
-                <button type="button"
-                    class="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-                    data-twe-ripple-init>
-                    Button
-                </button>
-            </div>
-            <div class="block max-w-sm rounded-lg bg-white bg-cover p-6 shadow-lg"
-                style="background-image: url('img/img-3.webp');">
-                <h5 class="mb-2 text-xl font-medium leading-tight  text-white ">
-                    Card title
-                </h5>
-                <p class="mb-4 text-base text-white ">
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </p>
-                <button type="button"
-                    class="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-                    data-twe-ripple-init>
-                    Button
-                </button>
-            </div>
-            <div class="block max-w-sm rounded-lg bg-white bg-cover p-6 shadow-lg"
-                style="background-image: url('img/img-3.webp');">
-                <h5 class="mb-2 text-xl font-medium leading-tight text-white ">
-                    Card title
-                </h5>
-                <p class="mb-4 text-base text-white ">
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </p>
-                <button type="button"
-                    class="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-                    data-twe-ripple-init>
-                    Button
-                </button>
+                <div class="pl-12">
+                    <h2 class="text-white text-2xl">Experticia en Gestión de Crisis</h2>
+                    <p class="peer mb-6 text-white">
+                        Simply Air Conditioning London are fixed system heating and air conditioning installation
+                        specialists. Because we’ve tested all heating and air conditioning unit manufacturers before
+                        using them there are no hidden surprises for our customers. We only supply air conditioning
+                        units from the leading manufacturers so any system we fit will be of the highest quality. We
+                        also thoroughly test every type of unit before supplying..
+                    </p>
+                    <p class="mb-6 text-white">
+                        We also provide tailored Air Conditioning installation packages. The Air Conditioning systems we
+                        install are all inverter driven therefor are energy efficient, stylish & silent both providing
+                        cooling and heating. At Zero Degree AC Limited we only use the best brands.
+                    </p>
+                    <h3 class="mb-4 font-semibold text-xl text-white">Conditioning installation packages</h3>
+                    <ul role="list" class="marker:text-white list-disc pl-5 space-y-3 text-white">
+                        <li>5 cups chopped Porcini mushrooms</li>
+                        <li>1/2 cup of olive oil</li>
+                        <li>3lb of celery</li>
+                    </ul>
+                </div>
             </div>
         </div>
-        @endsection
+    </div>
+</div>
+
+@endsection
