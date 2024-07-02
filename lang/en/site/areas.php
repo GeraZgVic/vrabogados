272a855472cc9e25e05d35def744c41a9722a447
Victor Zúñiga <gerazgvic@gmail.com>
2024-07-01 19:47:33 -0600

    Traducciones agregadas

--- lang/en/site/areas.php
+++ lang/en/site/areas.php
@@ -1,17 +1,27 @@
 <?php
 return [
-    'areas-servicios' => 'OUR SERVICES',
+    // Titles
+    'areas-servicios' => 'Our Services',
     'areas-derecho-civil' => 'Civil Law',
     'areas-derecho-penal' => 'Criminal Law',
     'areas-derecho-laboral' => 'Labor Law',
     'areas-derecho-administrativo' => 'Administrative Law',
     'areas-derecho-mercantil' => 'Commercial Law',
     'areas-derecho-tributario' => 'Tax Law',
-    'areas-derecho-descripcion' => 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life',
+    'areas-derecho-amparo' => 'Amparo Law',
+    'areas-derecho-constitucional' => 'Constitutional Law',
 
-    'areas-servicio-descripcion' => 'We have a deep understanding of our clients needs thanks to a solid experience, continuous training, and personalized treatment, all for the benefit of our clients peace of mind and security.',
-    'areas-practicas' => 'AREAS OF PRACTICE',
+    
+    'areas-practicas' => 'Areas Of Practice',
+    // Description
+    'derecho-civil-descripcion' => 'Expert legal advice on contracts, property, inheritances, and civil liability.',
+    'derecho-penal-descripcion' => 'Legal counsel on criminal cases, defense in trials, and management of criminal litigation.',
+    'derecho-laboral-descripcion' => 'Legal advice on labor relations, employment contracts, employee rights, and labor disputes.',
+    'derecho-administrativo-descripcion' => 'Specialized legal advice on administrative procedures, government regulations, and litigation before public entities.',
+    'derecho-mercantil-descripcion' => 'Legal counsel on commercial matters, commercial contracts, corporate law, and business litigation.',
+    'derecho-tributario-descripcion' => 'Legal advice on tax regulations, tax planning, defense against audits, and tax litigation.',
+    'derecho-amparo-descripcion' => 'Legal advice on the protection of constitutional rights, amparo remedies, and defense against violations of fundamental rights.',
+    'derecho-constitucional-descripcion' => 'Legal advice on fundamental rights, constitutional interpretation, and defense against constitutional violations.',
 
-    // list of areas
 
 ];
--- lang/es/site/areas.php
+++ lang/es/site/areas.php
@@ -1,5 +1,6 @@
 <?php
 return [
+    // Titulos
     'areas-servicios' => 'Nuestros Servicios',
     'areas-derecho-civil' => 'Derecho Civil',
     'areas-derecho-penal' => 'Derecho Penal',
@@ -7,9 +8,18 @@ return [
     'areas-derecho-administrativo' => 'Derecho Administrativo',
     'areas-derecho-mercantil' => 'Derecho Mercantil',
     'areas-derecho-tributario' => 'Derecho Tributario',
-    'areas-derecho-descripcion' => 'Incluso el todopoderoso Punto no tiene control sobre los textos ciegos; es una vida casi ortográfica.',
+    'areas-derecho-amparo' => 'Derecho Amparo',
+    'areas-derecho-constitucional' => 'Derecho Constitucional',
 
-    
-    'areas-servicio-descripcion' => 'Contamos con un profundo conocimiento de las necesidades de nuestros clientes gracias a una experiencia sólida, formación continua y un trato personalizado, todo ello en beneficio de la tranquilidad y seguridad de nuestros clientes.',
-    'areas-practicas' => 'AREAS DE EJERCICIO',
+
+    'areas-practicas' => 'Areas de Práctica',
+    // Descripciones
+    'derecho-civil-descripcion' => 'Asesoría legal experta en contratos, propiedad, herencias y responsabilidad civil.',
+    'derecho-penal-descripcion' => 'Asesoramiento legal en casos criminales, defensa en juicios y gestión de litigios penales.',
+    'derecho-laboral-descripcion' => 'Asesoría legal en relaciones laborales, contratos de trabajo, derechos de empleados y litigios laborales.',
+    'derecho-administrativo-descripcion' => 'Asesoría legal especializada en procedimientos administrativos, normativas gubernamentales y litigios ante entidades públicas.',
+    'derecho-mercantil-descripcion' => 'Asesoría legal en temas comerciales, contratos mercantiles, derecho societario y litigios empresariales.',
+    'derecho-tributario-descripcion' => 'Asesoría legal en normativas fiscales, planificación tributaria, defensa ante auditorías y litigios fiscales.',
+    'derecho-amparo-descripcion' => 'Asesoría legal en la protección de derechos constitucionales, recursos de amparo y defensa ante violaciones a derechos fundamentales.',
+    'derecho-constitucional-descripcion' => 'Asesoría legal en derechos fundamentales, interpretación constitucional y defensa ante violaciones constitucionales.',
 ];
